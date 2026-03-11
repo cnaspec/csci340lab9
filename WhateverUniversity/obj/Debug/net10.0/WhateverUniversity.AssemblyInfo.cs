77bb67d2516a7b39ffca2768949ca062a5335c75
Clodagh <nanglecc@hendrix.edu>
2026-03-11 17:51:07 +0000

    Scaffolding

--- WhateverUniversity/obj/Debug/net10.0/WhateverUniversity.AssemblyInfo.cs
+++ WhateverUniversity/obj/Debug/net10.0/WhateverUniversity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WhateverUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58a56412bc4c3797d5ed0dbb8ebc9be9a1504c37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb6fcf57509ed7475763696e53f4f72fc9caa104")]
 [assembly: System.Reflection.AssemblyProductAttribute("WhateverUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WhateverUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
