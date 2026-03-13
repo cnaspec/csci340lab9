78cf0655de49bcf34cdf14722bfb7851a3cb3f85
Clodagh <nanglecc@hendrix.edu>
2026-03-13 21:15:47 +0000

    Sort by Last Name, Age and Enrollment Date

--- WhateverUniversity/Pages/Students/Index.cshtml.cs
+++ WhateverUniversity/Pages/Students/Index.cshtml.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using WhateverUniversity.Data;
 using WhateverUniversity.Models;
 
-namespace WhateverUniversity.Pages_Students
+namespace WhateverUniversity.Pages.Students
 {
     public class IndexModel : PageModel
     {
@@ -19,11 +19,49 @@ namespace WhateverUniversity.Pages_Students
             _context = context;
         }
 
-        public IList<Student> Student { get;set; } = default!;
+        public string NameSort { get; set; }
+        public string DateSort { get; set; }
+        // Also need to sort by age
+        public string AgeSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
 
-        public async Task OnGetAsync()
+        public IList<Student> Students { get;set; }
+
+        public async Task OnGetAsync(string sortOrder)
         {
-            Student = await _context.Students.ToListAsync();
+            // using System;
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+            //Need to sort ny age also
+            AgeSort = sortOrder == "Age" ? "age_desc" : "Age";
+
+            IQueryable<Student> studentsIQ = from s in _context.Students
+                                            select s;
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    studentsIQ = studentsIQ.OrderByDescending(s => s.LastName);
+                    break;
+                case "Date":
+                    studentsIQ = studentsIQ.OrderBy(s => s.EnrollmentDate);
+                    break;
+                case "date_desc":
+                    studentsIQ = studentsIQ.OrderByDescending(s => s.EnrollmentDate);
+                    break;
+                case "Age":
+                    studentsIQ = studentsIQ.OrderBy(s => s.Age);
+                    break;
+                case "age_desc":
+                    studentsIQ = studentsIQ.OrderByDescending(s => s.Age);
+                    break;
+                default:
+                    studentsIQ = studentsIQ.OrderBy(s => s.LastName);
+                    break;
+            }
+
+            Students = await studentsIQ.AsNoTracking().ToListAsync();
         }
     }
 }
--- WhateverUniversity/obj/Debug/net10.0/WhateverUniversity.AssemblyInfo.cs
+++ WhateverUniversity/obj/Debug/net10.0/WhateverUniversity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WhateverUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b5b3ed9ca978db51a932e55d7ff4a2f08992cf9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+294d6b8fcd20bd173ea672f706839fac77ac8d6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WhateverUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WhateverUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
