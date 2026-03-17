50c1c02d238cad8e514a325d272111dae2996fbd
Clodagh <nanglecc@hendrix.edu>
2026-03-17 19:14:08 +0000

    Courses Create Page Update

--- WhateverUniversity/Pages/Courses/Create.cshtml.cs
+++ WhateverUniversity/Pages/Courses/Create.cshtml.cs
@@ -10,7 +10,7 @@ using WhateverUniversity.Models;
 
 namespace WhateverUniversity.Pages.Courses
 {
-    public class CreateModel : PageModel
+    public class CreateModel : DepartmentNamePageModel
     {
         private readonly WhateverUniversity.Data.SchoolContext _context;
 
@@ -21,7 +21,7 @@ namespace WhateverUniversity.Pages.Courses
 
         public IActionResult OnGet()
         {
-        ViewData["DepartmentID"] = new SelectList(_context.Departments, "DepartmentID", "DepartmentID");
+            PopulateDepartmentsDropDownList(_context);
             return Page();
         }
 
@@ -31,15 +31,21 @@ namespace WhateverUniversity.Pages.Courses
         // For more information, see https://aka.ms/RazorPagesCRUD.
         public async Task<IActionResult> OnPostAsync()
         {
-            if (!ModelState.IsValid)
+            var emptyCourse = new Course();
+
+            if (await TryUpdateModelAsync<Course>(
+                 emptyCourse,
+                 "course",   // Prefix for form value.
+                 s => s.CourseID, s => s.DepartmentID, s => s.Title, s => s.Credits))
             {
-                return Page();
+                _context.Courses.Add(emptyCourse);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
             }
 
-            _context.Courses.Add(Course);
-            await _context.SaveChangesAsync();
-
-            return RedirectToPage("./Index");
+            // Repopulate departments dropdown. emptyCourse.DepartmentID determines the selected item.
+            PopulateDepartmentsDropDownList(_context, emptyCourse.DepartmentID);
+            return Page();
         }
     }
 }
--- WhateverUniversity/obj/Debug/net10.0/WhateverUniversity.AssemblyInfo.cs
+++ WhateverUniversity/obj/Debug/net10.0/WhateverUniversity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WhateverUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+018a5c94e8c352a126b385d28a19af934a67bc0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16504580fcc9fa04aa300e6d44bd71e535164dff")]
 [assembly: System.Reflection.AssemblyProductAttribute("WhateverUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WhateverUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
